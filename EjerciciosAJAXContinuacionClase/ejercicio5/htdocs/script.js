1cca05113c9b105abab0d57844e09089c77a73a3
Fran <frankarroyop@gmail.com>
2021-11-12 08:46:11 +0100

    Arreglado Ejercicio 5

--- EjerciciosAJAXContinuacionClase/ejercicio5/htdocs/script.js
+++ EjerciciosAJAXContinuacionClase/ejercicio5/htdocs/script.js
@@ -10,29 +10,23 @@ window.addEventListener("load", (e) => {
         })
         .then(datos => {
             let respuesta = [];
-
             for (const provincia in datos) {
-                console.log(provincia);
-                console.log(datos[provincia]);
+                let opcionprovincia = document.createElement("option");
+                opcionprovincia.setAttribute("name", datos[provincia]);
+                opcionprovincia.setAttribute("value", datos[provincia]);
+                opcionprovincia.setAttribute("id", provincia);
+                opcionprovincia.innerHTML = datos[provincia]
+                provincias.appendChild(opcionprovincia);
             }
             // datos.forEach(provincia => {
             //     let codigo = provincia[0];
             //     let nombre = provincia[1];
             //     console.log(`CP: ${codigo} Nombre: ${nombre}`)
             // });
-            let listadeprovincias = respuesta;
-            console.log(listadeprovincias[0]);
-            for (let index = 1; index < listadeprovincias.length(); index++) {
-                let opcionprovincia = document.createElement("option");
-                opcionprovincia.setAttribute("name", listadeprovincias[index].children[1].textContent);
-                opcionprovincia.setAttribute("value", listadeprovincias[index].children[1].textContent);
-                opcionprovincia.setAttribute("id", listadeprovincias[index].children[0].textContent);
-                opcionprovincia.innerHTML = listadeprovincias[index].children[1].textContent;
-                provincias.appendChild(opcionprovincia);
-            }
         })
         .catch(console.log("Error: "));
 })
+
 let formulario = document.getElementById('formulario');
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -40,30 +34,29 @@ formulario.addEventListener("submit", (e) => {
     let provinciaseleccionada = document.getElementById("seleccionarprovincias").selectedOptions;
     let eleccion = provinciaseleccionada[0].id;
     datoformulario.append("provincia", eleccion);
-    let municipios = document.getElementById("seleccionarmunicipio");
-    const opciones = {
+    let listamunicipios = document.getElementById("seleccionarmunicipio");
+
+    const opcionesmunicipio = {
         method: 'POST',
         body: datoformulario
     }
-    fetch('http://ejercicio4.loc/cargaMunicipiosJSON.php', opciones)
+    fetch('http://ejercicio5.loc/cargaMunicipiosJSON.php', opcionesmunicipio)
         .then(response => {
             if (response.ok) {
-                return response.text();
+                return response.json();
             }
             return Promise.reject(response);
         })
-        .then(datos => {
-            const parser = new DOMParser();
-            const xml = parser.parseFromString(datos, "application/xml");
-            let listademunicipios = xml.getElementsByTagName('municipio');
-            for (let index = 0; index < listademunicipios.length; index++) {
-                console.log(listademunicipios[index]);
-                let opcionmunicpio = document.createElement("option");
-                opcionmunicpio.setAttribute("name", listademunicipios[index].children[1].textContent);
-                opcionmunicpio.setAttribute("value", listademunicipios[index].children[1].textContent);
-                opcionmunicpio.setAttribute("id", listademunicipios[index].children[0].textContent);
-                opcionmunicpio.innerHTML = listademunicipios[index].children[1].textContent;
-                municipios.appendChild(opcionmunicpio);
+        .then(datosmunicipio => {
+            for (const municipio in datosmunicipio) {
+                let opcionesmunicipio = document.createElement("option");
+                opcionesmunicipio.setAttribute("name", datosmunicipio[municipio]);
+                opcionesmunicipio.setAttribute("value", datosmunicipio[municipio]);
+                opcionesmunicipio.setAttribute("id", datosmunicipio);
+                opcionesmunicipio.innerHTML = datosmunicipio[municipio];
+                listamunicipios.appendChild(opcionesmunicipio);
             }
         })
+        .catch(console.log("Error: "));
+
 })
